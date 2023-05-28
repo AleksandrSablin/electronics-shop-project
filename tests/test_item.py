6afc41dcb34d17ea0c97dc60739c68a95e50425b
AleksandrSablin <sashuta-sablin@list.ru>
2023-05-28 15:55:32 +0300

    tests 100% coverage create requirements.txt

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,29 @@
+from src.item import Item
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+def test_item_init():
+
+    item = Item("Смартфон", 10000, 20)
+    assert item.name == "Смартфон"
+    assert item.price == 10000
+    assert item.quantity == 20
+    assert len(item.all) == 1
+
+def test_item_calculate_total_price():
+
+    item = Item("Смартфон", 10000, 20)
+    assert item.calculate_total_price() == 200000
+
+def test_apply_discount():
+
+    item = Item("Смартфон", 10000, 20)
+    item.apply_discount()
+    assert item.price == 10000
+
+    item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 8000
+
+
+
