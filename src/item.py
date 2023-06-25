bd6c49bbca309090889c29e2e878be41df5392db
AleksandrSablin <sashuta-sablin@list.ru>
2023-06-25 18:21:20 +0300

    Создан новый файл src/phone.py Создан в src/phone.py новый класс Phone Реализована возможность сложения экземпляров класса Phone и Item

--- src/item.py
+++ src/item.py
@@ -28,6 +28,11 @@ class Item:
     def __str__(self):
         return f'{self.name}'
 
+    def __add__(self, other):
+        if not isinstance(other, Item):
+            raise ValueError('Сложение недопустимо')
+        return int(self.quantity) + int(other.quantity)
+
     @property
     def name(self):
         return self.__name
